314ef3048ecea9127b798dfb005d7f2ed9ae24c0
Mariano García Yalungo <garcia.mariano@gmail.com>
2023-08-08 11:52:02 -0300

    add parallelism

--- download_s3_files.py
+++ download_s3_files.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import boto3
+import concurrent.futures
 
 from progress.bar import Bar
 
@@ -22,9 +23,19 @@ parser.add_argument("-b", "--bucket", help="Bucket name", required=True)
 parser.add_argument("-k", "--key", help="Access Key", default=ACCESS_KEY)
 parser.add_argument("-s", "--secret", help="AWS Secret", default=AWS_SECRET)
 
+parser.add_argument("-c", "--parallelism", help="Number of downloads in parallelism", default=1)
+
 parser = parser.parse_args()
 
-def descargar(bucket, download_folder, prefix, key, secret) -> int:
+def download_s3(bucket_session, key, download_folder):
+    file_name = os.path.basename(key)
+    f = f"{download_folder}/{file_name}"
+
+    with open(f, 'wb') as data:
+        bucket_session.download_fileobj(key, data)
+        
+
+def download(bucket, download_folder, prefix, key, secret, parallelism = 1) -> int:
     session = boto3.Session(aws_access_key_id=key, aws_secret_access_key=secret)
 
     s3 = session.resource("s3")
@@ -32,23 +43,20 @@ def descargar(bucket, download_folder, prefix, key, secret) -> int:
     my_bucket = s3.Bucket(bucket)
 
     l = my_bucket.objects.filter(Prefix=prefix)
+  
+    with concurrent.futures.ThreadPoolExecutor(max_workers=parallelism) as executor:
+        futures = [executor.submit(download_s3, my_bucket, file.key, download_folder) for file in l]
 
-    index = 0
-    for a in l: index+=1
+        index = 0
+        for _ in l: index+=1
+        b = Bar("Downloading...", max=index)
+        for _ in concurrent.futures.as_completed(futures):
+            b.next() 
 
-    b = Bar("Downloading...", max=index)
-
-    for file in l:  
-        file_name = os.path.basename(file.key)
-        f = f"{download_folder}/{file_name}"
-
-        with open(f, 'wb') as data:
-            my_bucket.download_fileobj(file.key, data)
-            b.next()
     return index
 
 def main():
-    total = descargar(parser.bucket, parser.download, parser.prefix, parser.key, parser.secret)
+    total = download(parser.bucket, parser.download, parser.prefix, parser.key, parser.secret, int(parser.parallelism))
 
     print(f"\n\n Downloaded {total} files")
 
